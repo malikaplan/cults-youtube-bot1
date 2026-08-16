9a73c79ca50a3501595dd9b6d0637203423d61e6
malikaplan <malikaplan@gmail.com>
2026-08-16 15:36:01 +0300

    Add files via upload

--- cults_to_youtube.py
+++ cults_to_youtube.py
@@ -591,7 +591,7 @@ def get_youtube_service():
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
                 CLIENT_SECRET_FILE,
-                ["https://www.googleapis.com/auth/youtube.upload"])
+                ["https://www.googleapis.com/auth/youtube.upload", "https://www.googleapis.com/auth/youtube.force-ssl"])
             creds = flow.run_local_server(port=0)
         with open(TOKEN_FILE, "wb") as f:
             pickle.dump(creds, f)
